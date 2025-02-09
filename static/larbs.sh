502dc4889e0a693568b8916c7e4c03981be60fc5
tiesinto <141479594+tiesinto@users.noreply.github.com>
2025-02-09 15:24:16 -0500

    comment out arken-fox-auto-update wrapper since ill need it to make my own wrapper

--- static/larbs.sh
+++ static/larbs.sh
@@ -201,56 +201,30 @@ vimplugininstall() {
 	sudo -u "$name" nvim -c "PlugInstall|q|q"
 }
 
-makeuserjs(){
-	# Get the Arkenfox user.js and prepare it.
-	arkenfox="$pdir/arkenfox.js"
-	overrides="$pdir/user-overrides.js"
-	userjs="$pdir/user.js"
-	ln -fs "/home/$name/.config/firefox/larbs.js" "$overrides"
-	[ ! -f "$arkenfox" ] && curl -sL "https://raw.githubusercontent.com/arkenfox/user.js/master/user.js" > "$arkenfox"
-	cat "$arkenfox" "$overrides" > "$userjs"
-	chown "$name:wheel" "$arkenfox" "$userjs"
-	# Install the updating script.
-	mkdir -p /usr/local/lib /etc/pacman.d/hooks
-	cp "/home/$name/.local/bin/qutebrowser-auto-delete" /usr/local/lib/
-	chown root:root /usr/local/lib/qutebrowser-auto-delete
-	chmod 755 /usr/local/lib/qutebrowser-auto-delete
-	# Trigger the update when needed via a pacman hook.
-	echo "[Trigger]
-Operation = Upgrade
-Type = Package
-Target = qutebrowser
-[Action]
-Description=Update qutebrowser to auto-delete history, cookies and cache on startup
-When=PostTransaction
-Exec=/usr/local/lib/qutebrowser-auto-delete" > /etc/pacman.d/hooks/arkenfox.hook
-}
-
-installffaddons(){
-	addonlist="ublock-origin decentraleyes istilldontcareaboutcookies vim-vixen"
-	addontmp="$(mktemp -d)"
-	trap "rm -fr $addontmp" HUP INT QUIT TERM PWR EXIT
-	IFS=' '
-	sudo -u "$name" mkdir -p "$pdir/extensions/"
-	for addon in $addonlist; do
-		if [ "$addon" = "ublock-origin" ]; then
-			addonurl="$(curl -sL https://api.github.com/repos/gorhill/uBlock/releases/latest | grep -E 'browser_download_url.*\.firefox\.xpi' | cut -d '"' -f 4)"
-		else
-			addonurl="$(curl --silent "https://addons.mozilla.org/en-US/firefox/addon/${addon}/" | grep -o 'https://addons.mozilla.org/firefox/downloads/file/[^"]*')"
-		fi
-		file="${addonurl##*/}"
-		sudo -u "$name" curl -LOs "$addonurl" > "$addontmp/$file"
-		id="$(unzip -p "$file" manifest.json | grep "\"id\"")"
-		id="${id%\"*}"
-		id="${id##*\"}"
-		mv "$file" "$pdir/extensions/$id.xpi"
-	done
-	chown -R "$name:$name" "$pdir/extensions"
-	# Fix a Vim Vixen bug with dark mode not fixed on upstream:
-	sudo -u "$name" mkdir -p "$pdir/chrome"
-	[ ! -f  "$pdir/chrome/userContent.css" ] && sudo -u "$name" echo ".vimvixen-console-frame { color-scheme: light !important; }
-#category-more-from-mozilla { display: none !important }" > "$pdir/chrome/userContent.css"
-}
+# makeuserjs(){
+# 	# Get the Arkenfox user.js and prepare it.
+# 	arkenfox="$pdir/arkenfox.js"
+# 	overrides="$pdir/user-overrides.js"
+# 	userjs="$pdir/user.js"
+# 	ln -fs "/home/$name/.config/firefox/larbs.js" "$overrides"
+# 	[ ! -f "$arkenfox" ] && curl -sL "https://raw.githubusercontent.com/arkenfox/user.js/master/user.js" > "$arkenfox"
+# 	cat "$arkenfox" "$overrides" > "$userjs"
+# 	chown "$name:wheel" "$arkenfox" "$userjs"
+# 	# Install the updating script.
+# 	mkdir -p /usr/local/lib /etc/pacman.d/hooks
+# 	cp "/home/$name/.local/bin/qutebrowser-auto-delete" /usr/local/lib/
+# 	chown root:root /usr/local/lib/qutebrowser-auto-delete
+# 	chmod 755 /usr/local/lib/qutebrowser-auto-delete
+# 	# Trigger the update when needed via a pacman hook.
+# 	echo "[Trigger]
+# Operation = Upgrade
+# Type = Package
+# Target = qutebrowser
+# [Action]
+# Description=Update qutebrowser to auto-delete history, cookies and cache on startup
+# When=PostTransaction
+# Exec=/usr/local/lib/qutebrowser-auto-delete" > /etc/pacman.d/hooks/arkenfox.hook
+# }
 
 finalize() {
 	whiptail --title "All done!" \
