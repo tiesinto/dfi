d98be6a463a32c490ab8e4c4dd2367d32105acbf
tiesinto <141479594+tiesinto@users.noreply.github.com>
2025-02-09 21:20:51 -0500

    use my dotfiles

--- static/dfi.sh
+++ static/dfi.sh
@@ -6,7 +6,7 @@
 
 ### OPTIONS AND VARIABLES ###
 
-dotfilesrepo="https://github.com/lukesmithxyz/voidrice.git"
+dotfilesrepo="https://github.com/tiesinto/boydrice.git"
 progsfile="https://raw.githubusercontent.com/tiesinto/dfi/master/static/progs.csv"
 aurhelper="yay"
 repobranch="master"
